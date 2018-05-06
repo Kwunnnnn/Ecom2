9e1fb49010cfda9977edddeeaa8f70cb0704c1e1
asama <asama589@gmail.com>
2018-05-06 13:22:56 +0700

    update authen

--- back-end/stock/models/stock_model.js
+++
@@ -1,7 +0,0 @@
-let mongoose = require('mongoose');
-let Schema = mongoose.Schema;
-
-let stockSchema = new Schema({
-   quality: {type: Number,require: true},
-   img: {type: [String],require: true}
-});
